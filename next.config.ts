e1a561a1315ee62a2557c6d5ed8bab3b672ffd36
Adelson Saguate <adelsonsaguate@acsun-mz.com>
2025-06-29 14:58:37 +0200

    Configurado a permissão das requições e resolvido o problema de CORS

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,14 @@ const nextConfig = {
   },
   // Importante para o Google Maps
   transpilePackages: ["@react-google-maps/api"],
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:8000/api/:path*",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
